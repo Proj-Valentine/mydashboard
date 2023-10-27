3656495fc30402637034d4b38c2ea3c3a8bafd72
VKAmpah <vkampah28@gmail.com>
2023-10-27 17:14:18 -0230

    Add Tooltip comp with settings button &Twind style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 
 import './App.css';
@@ -6,7 +9,23 @@ import './App.css';
 const App = () => {
   return (
     <div>
-     <h1 className='underline' text-3xl >APP</h1> 
+        <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style = {{zIndex:'1000'}}>
+                <TooltipComponent content= "Settings" position="Top">
+                    <botton type="button" 
+                    className="text-3xl p-3 
+                    hover:drop-shadow-xl
+                    hover:bg-light-gray text-white"
+                    style ={{background: 'blue',borderRadius:'50%'}}>
+                        <FiSettings/>
+                    </botton>
+                </TooltipComponent>
+
+            </div>
+
+        </div>
+        </BrowserRouter>
     </div>
   )
 }
