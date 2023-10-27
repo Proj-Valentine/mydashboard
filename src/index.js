a3b87d38fcdc489d6331feba5a51b7ac9e686828
VKAmpah <vkampah28@gmail.com>
2023-10-27 16:53:40 -0230

    setting up intial files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+
+// hook your react application to the root of the index file
+ReactDOM.render(<App />, document.getElementById('root') );
