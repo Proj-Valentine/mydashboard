c8e91092e1d0670cabf43bc569993069702a78c4
VKAmpah <vkampah28@gmail.com>
2023-10-27 23:58:22 -0230

    create components and applications needed

---
+++ src/components/ThemeSet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ThemeSet = () => {
+  return (
+    <div>
+      Themes
+    </div>
+  )
+}
+
+export default ThemeSet
---
+++ src/components/UserProflie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserProflie = () => {
+  return (
+    <div>
+      UserProfile
+    </div>
+  )
+}
+
+export default UserProflie
