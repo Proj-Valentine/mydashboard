a8dba2a3b4cc1c5706856cb9a014a44236a6f822
VKAmpah <vkampah28@gmail.com>
2023-10-31 18:36:13 -0230

    add pre data

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -8,6 +8,8 @@ const Editor = () => {
          <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
             <Header category="App" title="Editor"/>
             <RichTextEditorComponent>
+              {/* add pre defined  data But you can type anything/ delete to create empty editor */}
+              <EditorData/>
               <Inject services={[HtmlEditor, Toolbar,Image, Link, QuickToolbar]}/>
             </RichTextEditorComponent>
 
