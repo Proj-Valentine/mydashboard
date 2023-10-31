6bd2d2c00e10e23a99758929e5ff7ca0ba002dae
VKAmpah <vkampah28@gmail.com>
2023-10-31 16:27:14 -0230

    Create Empoyee page grid

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective,ColumnDirective,Page,Search,Inject,Toolbar } from "@syncfusion/ej2-react-grids";
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>
-      Employees
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category ="Page" title="Employees"/>
+      {/* creating the grid component to display fields and headers and data */}
+      <GridComponent 
+      dataSource={employeesData} 
+      //allow paging creates pagination so that all order items doesnt display on one page for long scrolling but rather pages to page
+      allowPaging
+      allowSorting
+      // enable this to bring the search box on the grid to search for elements// by importing Toolbar and injecting it
+      toolbar={['Search']}
+      width="auto">
+        <ColumnsDirective>
+        {employeesGrid.map((item,index)=>(
+          // ColumnDirective reads the data from the orders grid set up// which
+          // conntains an array of objects (fields and their properties)
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        <Inject services= {[Search, Page,Toolbar ]}/>
+      </GridComponent>
     </div>
   )
 }
