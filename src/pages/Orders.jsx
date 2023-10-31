fcc092a808e5c19f9a31a7a274a419784d8346cf
VKAmpah <vkampah28@gmail.com>
2023-10-31 16:07:12 -0230

    Add pagination feature and inject services(sort etc)

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -8,7 +8,12 @@ const Orders = () => {
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category ="Page" title="Orders"/>
       {/* creating the grid component to display fields and headers and data */}
-      <GridComponent id="gridcomp" dataSource={ordersData}>
+      <GridComponent 
+      id="gridcomp" 
+      dataSource={ordersData} 
+      //allow paging creates pagination so that all order items doesnt display on one page for long scrolling but rather pages to page
+      allowPaging
+      allowSorting>
         <ColumnsDirective>
         {ordersGrid.map((item,index)=>(
           // ColumnDirective reads the data from the orders grid set up// which
@@ -16,6 +21,7 @@ const Orders = () => {
           <ColumnDirective key={index} {...item}/>
         ))}
         </ColumnsDirective>
+        <Inject services= {[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]}/>
       </GridComponent>
     </div>
   )
