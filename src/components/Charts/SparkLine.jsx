811c31b16760f016a22cd92f2c2b423fde71629a
VKAmpah <vkampah28@gmail.com>
2023-11-02 03:26:10 -0230

    CLeanUp and update to Bar Chat

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { SparklineComponent,Inject,SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = ({id,height,width,color,data,type,currentColor}) => {
+
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
 
   return (
     <div>
@@ -30,6 +33,8 @@ const SparkLine = ({id,height,width,color,data,type,currentColor}) => {
      
     </div>
   )
+  }
 }
 
 export default SparkLine
+
