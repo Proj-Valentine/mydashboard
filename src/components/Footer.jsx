adf9da6667c5456e33b85b02cf600f25358ccf5d
VKAmpah <vkampah28@gmail.com>
2023-11-03 03:43:25 -0230

    fix rel="noreferrer" security risk in footer anchor tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
-import { FiMapPin } from 'react-icons/fi';
+
 
 const Footer = () => (
   <div className="mt-24 dark:text-gray-200 text-gray-700">
@@ -15,11 +15,11 @@ const Footer = () => (
         <div><a href="#top" style={{textDecoration:"none"}}>Top of Page</a></div>
         <div><p> PROFILE & PROJECTS</p>
         <SocialIcon  label="my video channel" network= "linkedin" url="https://www.linkedin.com/in/valentine-ampah-b92a0198" target="_blank" />
-        &nbsp;<span><a href="https://www.linkedin.com/in/valentine-ampah-b92a0198" target="_blank" style={{textDecoration:"none"}} className="mt-24 dark:text-gray-200 text-gray-700">LinkedIn</a></span>
-            &nbsp;&nbsp;<SocialIcon network = "github" url="https://github.com/Proj-Valentine/Python_Projects" target="_blank" bgColor="white" fgColor="black" />
-            &nbsp;<span><a href="https://github.com/Proj-Valentine/Python_Projects" target="_blank" style={{textDecoration:"none"}} className="mt-24 dark:text-gray-200 text-gray-700">PythonAutomation</a></span>
-            &nbsp;&nbsp;<SocialIcon network = "github" url="https://github.com/Proj-Valentine/mydashboard" target="_blank" bgColor="white" fgColor="black" />
-            &nbsp;<span><a href="https://github.com/Proj-Valentine/mydashboard" target="_blank" style={{textDecoration:"none"}} className="mt-24 dark:text-gray-200 text-gray-700">ReactDashboardApp</a></span>
+        &nbsp;<span><a href="https://www.linkedin.com/in/valentine-ampah-b92a0198" target="_blank"  rel="noreferrer" style={{textDecoration:"none"}} className="mt-24 dark:text-gray-200 text-gray-700">LinkedIn</a></span>
+            &nbsp;&nbsp;<SocialIcon network = "github" url="https://github.com/Proj-Valentine/Python_Projects" rel="noreferrer" target="_blank" bgColor="white" fgColor="black" />
+            &nbsp;<span><a href="https://github.com/Proj-Valentine/Python_Projects" target="_blank" rel="noreferrer" style={{textDecoration:"none"}} className="mt-24 dark:text-gray-200 text-gray-700">PythonAutomation</a></span>
+            &nbsp;&nbsp;<SocialIcon network = "github" url="https://github.com/Proj-Valentine/mydashboard" rel="noreferrer" target="_blank" bgColor="white" fgColor="black" />
+            &nbsp;<span><a href="https://github.com/Proj-Valentine/mydashboard" target="_blank" rel="noreferrer" style={{textDecoration:"none"}} className="mt-24 dark:text-gray-200 text-gray-700">ReactDashboardApp</a></span>
         </div>
     </div>
 
